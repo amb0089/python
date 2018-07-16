2b065074e4d32333038ff25c2f7092f5827d2c38
Austin Brown <amb0089@auburn.edu>
2018-07-16 17:14:23 -0500

    many new changes that work

--- GDD_calculator.py
+++ GDD_calculator.py
@@ -7,129 +7,149 @@ class TEModel(object):
         
         self.clipyld = []
         self.GDDlist = []
-        self.apps = 3
+        self.GDDacc = []
+        self.pastlist = []
+        self.pastacc = []
+        self.apps = 1
 
         
-    def GDDcal (self, GDD):
-        '''pulls yesterday's weather data from dark sky
+    def forecast (self):
+        '''pulls future weather data from dark sky, and calculates daily GDDs, future and previous day. Adds to list.
         '''
-        self.GDD = GDD
-        return self.GDD
+    
+        x =0
+        while x < 5:
+
+            self.GDDlist.append(input("enter forecast avg: "))
+            x+=1
+        
+        return self.GDDlist
+        
+    def forecastCal (self):
+        self.day1 = self.GDDacc.append(int(self.GDDlist[0]))
+        self.day2 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1]))
+        self.day3 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1])+int(self.GDDlist[2]))
+        self.day4 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1])+int(self.GDDlist[2])+int(self.GDDlist[3]))
+        self.day5 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1])+int(self.GDDlist[2])+int(self.GDDlist[3])+int(self.GDDlist[4]))
+       
         
     
+
+
+    def official (self):
         
-    def updateGDD (self):
+        self.pastlist.append(int(input("enter official avg temp of yesterday: ")))
         
-        self.GDDlist.append(self.GDD)
+        return self.pastlist
+
         
-                
+    def officialList(self):
+        
+        x=1
+        
+        if x < len(self.pastlist):        
+                      
+            total = sum(self.pastlist[0:x])
+            
+            self.pastacc.append(total)
+            x+=1
+            # self.day2 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1]))
+            # self.day3 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1])+int(self.GDDlist[2]))
+            # self.day4 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1])+int(self.GDDlist[2])+int(self.GDDlist[3]))
+            # self.day5 = self.GDDacc.append(int(self.GDDlist[0])+int(self.GDDlist[1])+int(self.GDDlist[2])+int(self.GDDlist[3])+int(self.GDDlist[4]))
+
+        return self.pastacc 
+
+    def officialCal (self):
+
+        self.totalacc = max(self.pastacc)
        
+        return self.totalacc              
+    
     def clipyield(self):
         '''Predicts the clipping yield based on GDD
         '''
         
         if self.apps==1:
-            yld = 2.42 * (2.71828 **(-self.GDD/264.1)) * math.sin((3.14159*(self.GDD+808.6))/871.7)
-        
-        elif self.apps>1 and max(self.GDDlist) <= 300:
+            yld = 2.42 * (2.71828 **(-self.totalacc/264.1)) * math.sin((3.14159*(self.totalacc+808.6))/871.7)
+        elif self.apps>1 and self.totalacc <= 300:
             yld = -0.60
-        elif self.apps>1 and max(self.GDDlist) > 300 and max(self.GDDlist) <=400:
+        elif self.apps>1 and self.totalacc > 300 and self.totalacc <=400:
             yld = -0.25
-        elif self.apps>1 and max(self.GDDlist) > 400 and max(self.GDDlist) <=600:
-            yld = -0.10   
-        elif self.apps>1 and max(self.GDDlist) > 600:
-            yld = 0     
-            
+        elif self.apps>1 and self.totalacc > 400 and self.totalacc <=600:
+            yld = -0.10 
+        elif self.apps>1 and self.totalacc > 600:
+            yld = 0 
+
+
             
         if yld > 0:
             self.yld = 0
         else:
             self.yld = yld   
     
-        return self.clipyld
+        return self.yld
 
     def updateyld (self):
         
         self.clipyld.append(self.yld)
 
         return self.clipyld
-        
+       
     def model(self):
         
-    
-        print(self.clipyld)
-        print(self.GDDlist)
-    
+   
         pylab.title('Yield Data')
-        pylab.plot(self.GDDlist, self.clipyld)
+        pylab.plot(self.pastacc, self.clipyld)
         pylab.show()
 
 
 
 green = TEModel()
-green.GDDcal(25)
+x=0
+while x < 5:
+
+    #green.forecast()
+    #green.forecastCal()
+    #green.updateGDD()
+    green.official()
+    green.officialList()
+    green.officialCal()
+    green.clipyield()
+    green.updateyld()
+    x +=1
+
+print(green.pastlist)
+print(green.pastacc)
+
+
+
+#green.model()
+
+#print(green.GDDlist)
+#print(green.GDDacc)
+print(green.totalacc)
+
+print(green.clipyld)
+
+
+
+
+'''
+green.forecast(50)
 green.updateGDD()
 green.clipyield()
 green.updateyld()
 green.model()
 
-green.GDDcal(50)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-green.GDDcal(100)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-
-green.GDDcal(200)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-
-
-green.GDDcal(300)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-
-green.GDDcal(400)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-
-green.GDDcal(500)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-
-
-green.GDDcal(600)
-green.updateGDD()
-green.clipyield()
-green.updateyld()
-green.model()
-
-green.GDDcal(700)
+green.forecast(100)
 green.updateGDD()
 green.clipyield()
 green.updateyld()
 green.model()
 
 
-green.GDDcal(800)
+green.forecast(200)
 green.updateGDD()
 green.clipyield()
 green.updateyld()
@@ -137,12 +157,55 @@ green.model()
 
 
 
-green.GDDcal(900)
+green.forecast(300)
 green.updateGDD()
 green.clipyield()
 green.updateyld()
 green.model()
 
+
+green.forecast(400)
+green.updateGDD()
+green.clipyield()
+green.updateyld()
+green.model()
+
+
+green.forecast(500)
+green.updateGDD()
+green.clipyield()
+green.updateyld()
+green.model()
+
+
+
+green.forecast(600)
+green.updateGDD()
+green.clipyield()
+green.updateyld()
+green.model()
+
+green.forecast(700)
+green.updateGDD()
+green.clipyield()
+green.updateyld()
+green.model()
+
+
+green.forecast(800)
+green.updateGDD()
+green.clipyield()
+green.updateyld()
+green.model()
+
+
+
+green.forecast(900)
+green.updateGDD()
+green.clipyield()
+green.updateyld()
+green.model()
+'''
 
 
 
